602d738bdfd4f617ddaa810d2d072c06553cb61a
sachse_t <thomas.sachse@ottogroup.com>
2016-03-04 14:28:53 +0100

    Plugin fuer HDLLV http_log-Dateien

---
+++ logstash-filter-hdllv-http-log/lib/logstash/filters/hdllv_http_log.rb
@@ -0,0 +1,63 @@
+# encoding: utf-8
+require "logstash/filters/base"
+require "logstash/namespace"
+require "time"
+
+# require File.dirname(__FILE__) + '/../helpers/' + 'dueb_helper.rb'
+
+# Der Filter dient zum Verarbeiten von errlog-Dateien
+class LogStash::Filters::HdllvHttpLog < LogStash::Filters::Base
+
+  # Setting the config_name here is required. This is how you
+  # configure this filter from your Logstash config.
+  #
+  # filter {
+  #   errlog {
+  #   }
+  # }
+  #
+  config_name "hdllv_http_log"
+  
+
+  public
+  def initialize(config = {})
+    super
+
+    # Der Filter kann nicht parallelsiert werden, da Folgezeilen
+    # bearbeiten werden sollen bzw. Beziehungen zwischen den Zeilen bestehen
+    @threadsafe = false
+  end
+
+  def register
+    # Add instance variables 
+    @datum    = '2000.01.01' 
+    @zeit     = '00:00:00' 
+    @hostname = 'X'
+
+  end # def register
+
+  public
+  def filter(event)
+
+    f = event["message"].split("\s")
+
+    if(f[0] =~ /^\d+\.\d+\.\d+$/)
+      # normale Zeile
+      @datum    = event["datum"] = f.shift
+      @zeit     = event["zeit"] = f.shift
+      @hostname = event["hostname"] = f.shift
+    else
+      # Folgezeile
+      event["datum"] = @datum
+      event["zeit"] = @zeit
+      event["hostname"] = @hostname
+    end
+
+    event["nachricht"] = f.join(" ")
+    event["@timestamp"] = LogStash::Timestamp.new(Time.strptime("#{event["datum"]} #{event["zeit"]}", '%Y.%m.%d %H:%M:%S'))
+
+
+    # filter_matched should go in the last line of our successful code
+    filter_matched(event)
+  end # def filter
+end # class LogStash::Filters::HdllvHttpLog
---
+++ logstash-filter-hdllv-http-log/spec/filters/hdllv_http_log_spec.rb
@@ -0,0 +1,54 @@
+# encoding: utf-8
+# https://github.com/logstash-plugins/logstash-filter-grok/blob/master/spec/filters/grok_spec.rb
+#
+require 'spec_helper'
+require "logstash/filters/hdllv_http_log"
+require 'time'
+
+describe LogStash::Filters::HdllvHttpLog do
+  describe "Felder splitten" do
+    let(:config) do <<-CONFIG
+      filter {
+        hdllv_http_log {
+        }
+      }
+    CONFIG
+    end
+
+    sample("2016.03.04 05:03:39 10.177.227.116 /akstat (GET)") do
+      insist { subject["datum"] } == "2016.03.04"
+      insist { subject["zeit"] } == "05:03:39"
+      insist { subject["hostname"] } == "10.177.227.116"
+      insist { subject["nachricht"] } == "/akstat (GET)"
+      insist { subject["@timestamp"] } == LogStash::Timestamp.new(Time.strptime('2016.03.04 05:03:39', '%Y.%m.%d %H:%M:%S'))
+    end
+  end
+
+  describe "Folgezeilen bearbeiten" do
+    let(:config) do <<-CONFIG
+      filter {
+        hdllv_http_log {
+        }
+      }
+    CONFIG
+    end
+
+    sample(["2016.03.04 05:03:39 10.177.227.116 /akstat (GET)","Folgezeile"]) do
+      expect(subject).to be_a(Array)
+      insist { subject.size } == 2
+
+      insist { subject[0]["datum"] } == "2016.03.04"
+      insist { subject[0]["zeit"] } == "05:03:39"
+      insist { subject[0]["hostname"] } == "10.177.227.116"
+      insist { subject[0]["nachricht"] } == "/akstat (GET)"
+      insist { subject[0]["@timestamp"] } == LogStash::Timestamp.new(Time.strptime('2016.03.04 05:03:39', '%Y.%m.%d %H:%M:%S'))
+
+      insist { subject[1]["datum"] } == "2016.03.04"
+      insist { subject[1]["zeit"] } == "05:03:39"
+      insist { subject[1]["hostname"] } == "10.177.227.116"
+      insist { subject[1]["nachricht"] } == "Folgezeile"
+      insist { subject[1]["@timestamp"] } == LogStash::Timestamp.new(Time.strptime('2016.03.04 05:03:39', '%Y.%m.%d %H:%M:%S'))
+    end
+  end
+
+end
